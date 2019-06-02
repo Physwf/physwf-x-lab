b99697542eaaa652d486e3195cd11685fa1ce38d
physwf <joe.physwf@hotmail.com>
2019-06-02 23:29:43 +0800

    Mesh

--- dx11demo/Mesh.cpp
+++ dx11demo/Mesh.cpp
@@ -1,6 +1,37 @@
 #include "Mesh.h"
 #include "fbxsdk.h"
 #include "log.h"
+#include <vector>
+
+void FillFbxArray(FbxNode* pNode, std::vector<FbxNode*>& pOutMeshArray)
+{
+	if (pNode->GetMesh())
+	{
+		pOutMeshArray.push_back(pNode);
+	}
+
+	for (int i = 0; i < pNode->GetChildCount(); ++i)
+	{
+		FillFbxArray(pNode->GetChild(i));
+	}
+}
+
+FbxAMatrix ComputeTotalMatrix(FbxScene* pScence, FbxNode* pNode)
+{
+	FbxAMatrix Geometry;
+	FbxVector4 Translation, Rotation, Scaling;
+	Translation = pNode->GetGeometricTranslation(FbxNode::eSourcePivot);
+	Rotation = pNode->GetGeometricRotation(FbxNode::eSourcePivot);
+	Scaling = pNode->GetGeometricScaling(FbxNode::eSourcePivot);
+	Geometry.SetT(Translation);
+	Geometry.SetR(Rotation);
+	Geometry.SetS(Scaling);
+
+	FbxAMatrix& GlobalTransform = pScence->GetAnimationEvaluator()->GetNodeGlobalTransform(pNode);
+
+
+
+}
 
 void Mesh::ImportFromFBX(const char* pFileName)
 {
@@ -21,14 +52,103 @@ void Mesh::ImportFromFBX(const char* pFileName)
 	lImporter->Import(lScene);
 	lImporter->Destroy();
 
-	FbxNode* lRootNode = lScene->GetRootNode();
-	if (lRootNode)
+	std::vector<FbxNode*> lOutMeshArray;
+	FillFbxArray(lScene->GetRootNode(), lOutMeshArray);
+
+	FbxGeometryConverter* GeometryConverter = new FbxGeometryConverter(lFbxManager);
+
+	for (auto i = 0; i < lOutMeshArray.size(); ++i)
 	{
-		int SubMeshCount = lRootNode->GetMaterialCount();
-		for (int i = 0; i < SubMeshCount; ++i)
+		FbxNode* lNode = lOutMeshArray[i];
+
+		if (lNode->GetMesh())
 		{
-			FbxSurfaceMaterial* lMaterial = lRootNode->GetMaterial(i);
+			FbxMesh* lMesh = lNode->GetMesh();
+
+			lMesh->RemoveBadPolygons();
+
+			if (!lMesh->IsTriangleMesh())
+			{
+				FbxNodeAttribute* ConvertedNode = GeometryConverter->Triangulate(lMesh,true);
+
+				if(ConvertedNode!=NULL && ConvertedNode->GetAttributeType() == FbxNodeAttribute::eMesh)
+				{
+					lMesh = ConvertedNode->GetNode()->GetMesh();
+				}
+				else
+				{
+					X_LOG("Cant triangulate!");
+				}
+			}
+
+			FbxLayer* BaseLayer = lMesh->GetLayer(0);
+
+			bool bSmootingAvaliable = false;
+			FbxLayerElementSmoothing* SmoothingInfo = BaseLayer->GetSmoothing();
+			FbxLayerElement::EReferenceMode SmoothingReferenceMode(FbxLayerElement::eDirect);
+			FbxLayerElement::EMappingMode SmoothingMappingMode(FbxLayerElement::eByEdge);
+			if (SmoothingInfo)
+			{
+				if (SmoothingInfo->GetMappingMode() == FbxLayerElement::eByPolygon)
+				{
+					GeometryConverter->ComputeEdgeSmoothingFromPolygonSmoothing(lMesh, 0);
+				}
+
+				if (SmoothingInfo->GetMappingMode() == FbxLayerElement::eByEdge)
+				{
+					bSmootingAvaliable = true;
+				}
+
+				SmoothingReferenceMode = SmoothingInfo->GetReferenceMode();
+				SmoothingMappingMode = SmoothingInfo->GetMappingMode();
+			}
+
+			FbxLayerElementVertexColor* LayerElementVertexColor = BaseLayer->GetVertexColors();
+			FbxLayerElement::EReferenceMode VertexColorReferenceMode(FbxLayerElement::eDirect);
+			FbxLayerElement::EMappingMode VertexColorMappingMode(FbxLayerElement::eByControlPoint);
+			if (LayerElementVertexColor)
+			{
+				VertexColorMappingMode = LayerElementVertexColor->GetReferenceMode();
+				VertexColorMappingMode = LayerElementVertexColor->GetMappingMode();
+			}
+
+			FbxLayerElementNormal* LayerElementNormal = BaseLayer->GetNormals();
+			FbxLayerElementTangent* LayerElementTangent = BaseLayer->GetTangents();
+			FbxLayerElementBinormal* LayerElementBinormal = BaseLayer->GetBinormals();
+
+			bool bHasNTBInfomation = LayerElementNormal && LayerElementTangent && LayerElementBinormal;
+
+			FbxLayerElement::EReferenceMode NormalReferenceMode(FbxLayerElement::eDirect);
+			FbxLayerElement::EMappingMode NormalMappingMode(FbxLayerElement::eByControlPoint);
+			if (LayerElementNormal)
+			{
+				NormalReferenceMode = LayerElementNormal->GetReferenceMode();
+				NormalMappingMode = LayerElementNormal->GetMappingMode();
+			}
+
+			FbxLayerElement::EReferenceMode TangentReferenceMode(FbxLayerElement::eDirect);
+			FbxLayerElement::EMappingMode TangentMappingMode(FbxLayerElement::eByControlPoint);
+			if (LayerElementTangent)
+			{
+				TangentReferenceMode = LayerElementTangent->GetReferenceMode();
+				TangentMappingMode = LayerElementTangent->GetMappingMode();
+			}
+
+			FbxLayerElement::EReferenceMode BinormalReferenceMode(FbxLayerElement::eDirect);
+			FbxLayerElement::EMappingMode BinormalMappingMode(FbxLayerElement::eByControlPoint);
+			if (LayerElementBinormal)
+			{
+				BinormalReferenceMode = LayerElementBinormal->GetReferenceMode();
+				BinormalMappingMode = LayerElementBinormal->GetMappingMode();
+			}
+
+			FbxAMatrix TotalMatrix;
+			FbxAMatrix TotalMatrixForNormal;
+
 		}
 	}
+
+	delete GeometryConverter;
+	GeometryConverter = nullptr;
 }
 
