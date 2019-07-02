489fa05319c5fc2289ffc7c3af4184ba496f8d67
physwf <joe.physwf@hotmail.com>
2019-07-02 23:32:59 +0800

    PT

--- path_tracing/path_tracing.cpp
+++ path_tracing/path_tracing.cpp
@@ -27,6 +27,19 @@ struct Vec
 	}
 };
 
+Vec operator*(float fValue , const Vec& InVec)
+{
+	return { fValue*InVec.X, fValue*InVec.Y,  fValue*InVec.Y };
+}
+
+struct Color
+{
+	float R, G, B;
+
+	static Color Black = { 0.0f,0.0f,0.0f };
+	static Color White = { 1.0f,1.0f,1.0f };
+};
+
 struct Ray
 {
 	Vec Position;
@@ -46,10 +59,8 @@ struct Square
 
 	float Width;
 
-	bool Intersect(const Ray& InRay,Vec OutPosition) const
+	bool Intersect(const Ray& InRay,Vec& OutPosition) const
 	{
-// 		Vec Dir1 = InRay.Position - Center;
-// 		if (Dir1*InRay.Dir < 0) return false;
 		float fDenominator = InRay.Dir * Normal;
 		if (fDenominator <= 0.00001f) return false;
 		float t = (Center - InRay.Position)*Normal / fDenominator;
@@ -70,9 +81,18 @@ struct Sphere
 	Vec Position;
 	float fRadius;
 
-	bool Intersect(const Ray& InRay, Vec OutPosition) const
+	bool Intersect(const Ray& InRay, Vec& OutPosition) const
 	{
-
+		///https://en.wikipedia.org/wiki/Line-sphere_intersection
+		Vec PositionVec = InRay.Position - Position;
+		float fValue = InRay.Dir*PositionVec;
+		float fLen = PositionVec.Length();
+		float fPart2 = fValue * fValue - (fLen * fLen - fRadius * fRadius);
+		if (fPart2 < 0.0f) return false;
+		float fPart1 = -(InRay.Dir*PositionVec);
+		//float t1 = fPart1 + fPart2;
+		float t2 = fPart1 - fPart2;
+		OutPosition = InRay.Position + t2 * InRay.Dir;
 	}
 };
 
@@ -86,7 +106,9 @@ struct Scene
 	Square Walls[5];
 	Sphere Ball[1];
 
-	Vec Radiance(Ray InRay)
+
+
+	Color Radiance(Ray InRay)
 	{
 
 	}
