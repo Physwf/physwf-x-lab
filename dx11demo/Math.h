c2d1298b183f4a933b18d2c1b5dd03f28533fb4c
physwf <joe.physwf@hotmail.com>
2019-12-25 11:32:17 +0800

    Math

--- dx11demo/Math.h
+++ dx11demo/Math.h
@@ -1,56 +1,112 @@
 #pragma once
 
+#include <initializer_list>
+#include <cassert>
+#include <cmath>
+
+typedef	signed char			int8;	
+typedef signed short int	int16;	
+typedef signed int	 		int32;	
+typedef signed long long	int64;		
+
 typedef unsigned char		uint8;
 typedef unsigned short		uint16;
 typedef unsigned int		uint32;
 typedef unsigned long long	uint64;
 
-struct Vector4
+struct alignas(16) Vector4
 {
 	float X, Y, Z, W;
 
-	Vector4 operator+(const Vector4& rhs);
-	Vector4 operator-(const Vector4& rhs);
-	Vector4 operator*(const Vector4& rhs);
-	Vector4 operator*(float Value);
-	Vector4 operator/(float Value);
-	Vector4 operator^(const Vector4& rhs);
+	Vector4(): X(0.0f), Y(0.0f), Z(0.0f), W(0.0f) {}
+	explicit Vector4(float V): X(V), Y(V), Z(V), W(V) {}
+	Vector4(float InX, float InY, float InZ, float InW) : X(InX), Y(InY), Z(InZ), W(InW) {}
+	Vector4(std::initializer_list<float> list);
+
+	Vector4 operator+(const Vector4& rhs) const;
+	Vector4 operator-(const Vector4& rhs) const;
+	Vector4 operator*(const Vector4& rhs) const;
+	Vector4 operator*(float Value) const;
+	Vector4 operator/(float Value) const;
+	Vector4 operator^(const Vector4& rhs) const;
 	Vector4 operator+=(const Vector4& rhs);
 	Vector4 operator-=(const Vector4& rhs);
 	Vector4 operator*=(const Vector4& rhs);
 	Vector4 operator*=(float Value);
 	Vector4 operator/=(float Value);
+	Vector4 operator-() const;
 
-	bool operator==(const Vector4& rhs);
-	bool operator!=(const Vector4& rhs);
+	bool operator==(const Vector4& rhs) const;
+	bool operator!=(const Vector4& rhs) const;
+
+	float& operator[](int32 Index);
+	float operator[](int32 Index) const;
+
+	friend inline float Dot3(const Vector4& lhs, const Vector4& rhs)
+	{
+		return lhs.X * rhs.X + lhs.Y * rhs.Y + lhs.Z * rhs.Z;
+	}
+	friend inline float Dot4(const Vector4& lhs, const Vector4& rhs)
+	{
+		return lhs.X * rhs.X + lhs.Y * rhs.Y + lhs.Z * rhs.Z + lhs.W * rhs.W;
+	}
+	friend inline Vector4 operator*(float Value, const Vector4& rhs)
+	{
+		return rhs.operator*(Value);
+	}
 };
 
-inline Vector4 Vector4::operator+(const Vector4& rhs)
+inline Vector4::Vector4(std::initializer_list<float> list)
+{
+	int32 i = 0;
+	for (auto&& v : list)
+	{
+		(*this)[i] = v;
+		if(i++ > 3) break;
+	}
+}
+
+inline Vector4 Vector4::operator+(const Vector4& rhs) const
 {
 	return { X + rhs.X, Y + rhs.Y, Z + rhs.Z, W + rhs.W };
 }
 
-inline Vector4 Vector4::operator-(const Vector4& rhs)
+inline Vector4 Vector4::operator-(const Vector4& rhs) const
 {
 	return { X - rhs.X, Y - rhs.Y, Z - rhs.Z, W - rhs.W };
 }
 
-inline Vector4 Vector4::operator*(const Vector4& rhs)
+inline Vector4 Vector4::operator-() const
+{
+	return { -X,-Y,-Z,-W };
+}
+
+inline float& Vector4::operator[](int32 Index)
+{
+	return (&X)[Index];
+}
+
+inline float Vector4::operator[](int32 Index) const
+{
+	return (&X)[Index];
+}
+
+inline Vector4 Vector4::operator*(const Vector4& rhs) const
 {
 	return { X * rhs.X, Y * rhs.Y, Z * rhs.Z, W * rhs.W };
 }
 
-inline Vector4 Vector4::operator*(float Value)
+inline Vector4 Vector4::operator*(float Value) const
 {
 	return { X * Value, Y * Value, Z * Value, W * Value };
 }
 
-inline Vector4 Vector4::operator/(float Value)
+inline Vector4 Vector4::operator/(float Value) const
 {
 	return { X / Value, Y / Value, Z / Value, W / Value };
 }
 
-inline Vector4 Vector4::operator^(const Vector4& rhs)
+inline Vector4 Vector4::operator^(const Vector4& rhs) const
 {
 	return 
 	{
@@ -92,25 +148,325 @@ inline Vector4 Vector4::operator/=(float Value)
 	return *this;
 }
 
-inline bool Vector4::operator==(const Vector4& rhs)
+inline bool Vector4::operator==(const Vector4& rhs) const
 {
 	return (X == rhs.X && Y == rhs.Y && Z == rhs.Z && W == rhs.W);
 }
 
-inline bool Vector4::operator!=(const Vector4& rhs)
+inline bool Vector4::operator!=(const Vector4& rhs) const
 {
 	return (X != rhs.X || Y != rhs.Y || Z != rhs.Z || W != rhs.W);
 }
 
+
 struct Vector
 {
 	float X, Y, Z;
+
+	Vector() : X(0.0f), Y(0.0f), Z(0.0f) {}
+	explicit Vector(float V) : X(V), Y(V), Z(V) {}
+	Vector(float InX, float InY, float InZ) : X(InX), Y(InY), Z(InZ) {}
+	Vector(std::initializer_list<float> list);
+
+	Vector operator+(const Vector& rhs) const;
+	Vector operator-(const Vector& rhs) const;
+	Vector operator*(const Vector& rhs) const;
+	Vector operator*(float Value) const;
+	Vector operator/(float Value) const;
+	Vector operator^(const Vector& rhs) const;
+	float operator|(const Vector& V) const;
+	Vector operator+=(const Vector& rhs);
+	Vector operator-=(const Vector& rhs);
+	Vector operator*=(const Vector& rhs);
+	Vector operator*=(float Value);
+	Vector operator/=(float Value);
+	Vector operator-() const;
+
+	bool operator==(const Vector& rhs) const;
+	bool operator!=(const Vector& rhs) const;
+
+	float& operator[](int32 Index);
+	float operator[](int32 Index) const;
+
+	inline void Normalize();
+
+	friend inline Vector operator*(float Value, const Vector& rhs)
+	{
+		return rhs.operator*(Value);
+	}
 };
+
+inline Vector::Vector(std::initializer_list<float> list)
+{
+	int32 i = 0;
+	for (auto&& v : list)
+	{
+		(*this)[i] = v;
+		if (i++ > 2) break;
+	}
+}
+
+inline Vector Vector::operator+(const Vector& rhs) const
+{
+	return { X + rhs.X, Y + rhs.Y, Z + rhs.Z };
+}
+
+inline Vector Vector::operator-(const Vector& rhs) const
+{
+	return { X - rhs.X, Y - rhs.Y, Z - rhs.Z };
+}
+
+inline Vector Vector::operator*(const Vector& rhs) const
+{
+	return { X * rhs.X, Y * rhs.Y, Z * rhs.Z };
+}
+
+inline Vector Vector::operator*(float Value) const
+{
+	return { X * Value, Y * Value, Z * Value };
+}
+
+inline Vector Vector::operator/(float Value) const
+{
+	return { X / Value, Y / Value, Z / Value };
+}
+
+inline Vector Vector::operator+=(const Vector& rhs)
+{
+	X += rhs.X; Y += rhs.Y; Z += rhs.Z;;
+	return *this;
+}
+
+inline Vector Vector::operator-=(const Vector& rhs)
+{
+	X -= rhs.X; Y -= rhs.Y; Z -= rhs.Z;
+	return *this;
+}
+
+inline Vector Vector::operator*=(const Vector& rhs)
+{
+	X *= rhs.X; Y *= rhs.Y; Z *= rhs.Z;;
+	return *this;
+}
+
+inline Vector Vector::operator*=(float Value)
+{
+	X *= Value; Y *= Value; Z *= Value;
+	return *this;
+}
+
+inline Vector Vector::operator/=(float Value)
+{
+	X /= Value; Y /= Value; Z /= Value;
+	return *this;
+}
+
+inline Vector Vector::operator-() const
+{
+	return { -X,-Y,-Z };
+}
+
+inline bool Vector::operator!=(const Vector& rhs) const
+{
+	return (X != rhs.X || Y != rhs.Y || Z != rhs.Z);
+}
+
+inline bool Vector::operator==(const Vector& rhs) const
+{
+	return (X == rhs.X && Y == rhs.Y && Z == rhs.Z);
+}
+
+
+inline Vector Vector::operator^(const Vector& rhs) const
+{
+	return
+	{
+		Y * rhs.Z - Z * rhs.Y,
+		Z * rhs.X - X * rhs.Z,
+		X * rhs.Y - Y * rhs.X,
+	};
+}
+
+inline float Vector::operator|(const Vector& V) const
+{
+	return X * V.X + Y * V.Y + Z * V.Z;
+}
+
+inline float& Vector::operator[](int32 Index)
+{
+	return (&X)[Index];
+}
+
+inline float Vector::operator[](int32 Index) const
+{
+	return (&X)[Index];
+}
+
+inline void Vector::Normalize()
+{
+	float InvSqrt = 1.0f / sqrtf(X * X + Y * Y + Z * Z);
+	X *= InvSqrt;
+	Y *= InvSqrt;
+	Z *= InvSqrt;
+}
 
 struct Vector2
 {
 	float X, Y;
+
+	Vector2() : X(0.0f), Y(0.0f) {}
+	explicit Vector2(float V) : X(V), Y(V) {}
+	Vector2(float InX, float InY) : X(InX), Y(InY) {}
+	Vector2(std::initializer_list<float> list);
+
+	Vector2 operator+(const Vector2& rhs) const;
+	Vector2 operator-(const Vector2& rhs) const;
+	Vector2 operator*(const Vector2& rhs) const;
+	Vector2 operator*(float Value) const;
+	Vector2 operator/(float Value) const;
+	float operator^(const Vector2& rhs) const;
+	float operator|(const Vector2& V) const;
+	Vector2 operator+=(const Vector2& rhs);
+	Vector2 operator-=(const Vector2& rhs);
+	Vector2 operator*=(const Vector2& rhs);
+	Vector2 operator*=(float Value);
+	Vector2 operator/=(float Value);
+	Vector2 operator-() const;
+
+	bool operator==(const Vector2& rhs) const;
+	bool operator!=(const Vector2& rhs) const;
+
+	float& operator[](int32 Index);
+	float operator[](int32 Index) const;
+
+	void Normalize();
+	float Size();
+	float SizeSquared();
+
+	friend inline Vector2 operator*(float Value, const Vector2& rhs)
+	{
+		return rhs.operator*(Value);
+	}
 };
+
+
+inline Vector2::Vector2(std::initializer_list<float> list)
+{
+	int32 i = 0;
+	for (auto&& v : list)
+	{
+		(*this)[i] = v;
+		if (i++ > 1) break;
+	}
+}
+
+inline Vector2 Vector2::operator+(const Vector2& rhs) const
+{
+	return { X + rhs.X, Y + rhs.Y };
+}
+
+inline Vector2 Vector2::operator-(const Vector2& rhs) const
+{
+	return { X - rhs.X, Y - rhs.Y };
+}
+
+inline Vector2 Vector2::operator*(const Vector2& rhs) const
+{
+	return { X * rhs.X, Y * rhs.Y };
+}
+
+inline Vector2 Vector2::operator*(float Value) const
+{
+	return { X * Value, Y * Value };
+}
+
+inline Vector2 Vector2::operator/(float Value) const
+{
+	return { X / Value, Y / Value};
+}
+
+inline Vector2 Vector2::operator+=(const Vector2& rhs)
+{
+	X += rhs.X; Y += rhs.Y;
+	return *this;
+}
+
+inline Vector2 Vector2::operator-=(const Vector2& rhs)
+{
+	X -= rhs.X; Y -= rhs.Y;
+	return *this;
+}
+
+inline Vector2 Vector2::operator*=(const Vector2& rhs)
+{
+	X *= rhs.X; Y *= rhs.Y;
+	return *this;
+}
+
+inline Vector2 Vector2::operator*=(float Value)
+{
+	X *= Value; Y *= Value;
+	return *this;
+}
+
+inline Vector2 Vector2::operator/=(float Value)
+{
+	X /= Value; Y /= Value;
+	return *this;
+}
+
+inline Vector2 Vector2::operator-() const
+{
+	return { -X,-Y };
+}
+
+inline bool Vector2::operator!=(const Vector2& rhs) const
+{
+	return (X != rhs.X || Y != rhs.Y);
+}
+
+inline bool Vector2::operator==(const Vector2& rhs) const
+{
+	return (X == rhs.X && Y == rhs.Y);
+}
+
+
+inline float Vector2::operator^(const Vector2& rhs) const
+{
+	return X * rhs.Y - Y * rhs.X;
+}
+
+inline float Vector2::operator|(const Vector2& V) const
+{
+	return X * V.X + Y * V.Y;
+}
+
+inline float& Vector2::operator[](int32 Index)
+{
+	return (&X)[Index];
+}
+
+inline float Vector2::operator[](int32 Index) const
+{
+	return (&X)[Index];
+}
+
+inline void Vector2::Normalize()
+{
+	float InvSqrt = 1.0f / sqrtf(X * X + Y * Y);
+	X *= InvSqrt;
+	Y *= InvSqrt;
+}
+
+inline float Vector2::Size()
+{
+	return sqrtf(X * X + Y * Y);
+}
+
+inline float Vector2::SizeSquared()
+{
+	return X * X + Y * Y;
+}
 
 struct LinearColor
 {
