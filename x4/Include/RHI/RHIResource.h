238904b1075801fcca2342eb30411e9e0c0a5585
physwf <joe.physwf@hotmail.com>
2020-04-21 18:31:43 +0800

    rhi

--- x4/Include/RHI/RHIResource.h
+++ x4/Include/RHI/RHIResource.h
@@ -3,16 +3,93 @@
 #include "X4.h"
 
 #include "CoreTypes.h"
+#include "Misc/AssertionMacros.h"
+#include "HAL/UnrealMemory.h"
+#include "Containers/Array.h"
+#include "Misc/Crc.h"
+#include "Containers/UnrealString.h"
+#include "HAL/ThreadSafeCounter.h"
+#include "RHIDefinitions.h"
+#include "Templates/RefCounting.h"
+#include "PixelFormat.h"
 
 class FRHIResource
 {
 public:
 	FRHIResource(bool InbDoNotDeferDelete = false)
+		: MarkedForDelete(0)
+		, bDoNotDeferDelete(InbDoNotDeferDelete)
+		, bCommitted(true)
 	{
+	}
 
+	FORCEINLINE_DEBUGGABLE uint32 AddRef() const
+	{
+		int32 NewValue = NumRefs.Increment();
+		checkSlow(NewValue > 0);
+		return uint32(NewValue);
+	}
+	FORCEINLINE_DEBUGGABLE uint32 Release() const
+	{
+		int32 NewValue = NumRefs.Decrement();
+		if (NewValue == 0)
+		{
+			delete this;
+		}
+		checkSlow(NewValue >= 0);
+		return uint32(NewValue);
+	}
+	FORCEINLINE_DEBUGGABLE uint32 GetRefCount() const
+	{
+		int32 CurrentValue = NumRefs.GetValue();
+		checkSlow(CurrentValue >= 0);
+		return uint32(CurrentValue);
+	}
+
+	// Transient resource tracking
+	// We do this at a high level so we can catch errors even when transient resources are not supported
+	void SetCommitted(bool bInCommitted)
+	{
+		//check(IsInRenderingThread());
+		bCommitted = bInCommitted;
+	}
+	bool IsCommitted() const
+	{
+		//check(IsInRenderingThread());
+		return bCommitted;
 	}
 private:
+	mutable FThreadSafeCounter NumRefs;
+	mutable int32 MarkedForDelete;
+	bool bDoNotDeferDelete;
+	bool bCommitted;
+
 
 };
 
 class FRHISamplerState : public FRHIResource {};
+class FRHIRasterizerState : public FRHIResource
+{
+public:
+	virtual bool GetInitializer(struct FRasterizerStateInitializerRHI& Init) { return false; }
+};
+class FRHIDepthStencilState : public FRHIResource
+{
+public:
+	virtual bool GetInitializer(struct FRasterizerStateInitializerRHI& Init) { return false; }
+};
+class FRHIBlendState : public FRHIResource
+{
+public:
+	virtual bool GetInitializer(struct FRasterizerStateInitializerRHI& Init) { return false; }
+};
+
+typedef TArray<struct FVertexElement, TFixedAllocator<MaxVertexElementCount> > FVertexDeclarationElementList;
+class FRHIVertexDeclaration : public FRHIResource
+{
+public:
+	virtual bool GetInitializer(FVertexDeclarationElementList& Init) { return false; }
+};
+
+class FRHIBoundShaderState : public FRHIResource {};
+
