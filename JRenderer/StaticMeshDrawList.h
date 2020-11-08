df94cd96c8266b3336e04d5bb33315028748604f
physwf <joe.physwf@hotmail.com>
2020-11-08 14:29:33 +0800

    deprecated

--- JRenderer/StaticMeshDrawList.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-template <typename DrawingPolicyType>
-class TStaticMeshDrawList
-{
-public:
-	void AddMesh();
-};
--- JRenderer/main.cpp
+++ JRenderer/main.cpp
@@ -10,13 +10,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
-	JApplication App(hInstance);
-	App.Initialize();
-	App.ShowMainWindow(nCmdShow);
 	int nParam = 0;
 	while (true)
 	{
-		nParam = App.Loop();
 	}
 	return nParam;
 }
