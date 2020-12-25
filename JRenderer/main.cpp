79fc01e5396d43c6984cda2762dccd1cf407660c
physwf <joe.physwf@hotmail.com>
2020-12-25 16:32:48 +0800

    dx12

--- JRenderer/main.cpp
+++ JRenderer/main.cpp
@@ -1,6 +1,15 @@
-#include "framework.h"
 #include "d3d12demo.h"
-#include "Application.h"
+
+LRESULT CALLBACK WindowProc(HWND hWnd,
+	UINT message,
+	WPARAM wParam,
+	LPARAM lParam);
+
+HWND g_hWind = NULL;
+LONG WindowWidth = 1920;
+LONG WindowHeight = 1080;
+
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -10,9 +19,104 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
-	int nParam = 0;
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.cbSize = sizeof WNDCLASSEX;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpszClassName = L"WindowClass1";
+
+	RegisterClassEx(&wc);
+
+	RECT wr = { 0,0,WindowWidth,WindowHeight };
+	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+
+	g_hWind = CreateWindowEx
+	(
+		NULL,
+		L"WindowClass1",
+		L"dx11demo",
+		WS_OVERLAPPEDWINDOW,
+		300,
+		300,
+		wr.right - wr.left,
+		wr.bottom - wr.top,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ShowWindow(g_hWind, nCmdShow);
+
+	D3D12Demo Demo(g_hWind);
+	Demo.Initialize();
+
+	MSG msg;
 	while (true)
 	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+			if (msg.message == WM_QUIT)
+			{
+				break;
+			}
+			continue;
+		}
+		{
+			Demo.Render();
+			Sleep(1);
+		}
+
 	}
-	return nParam;
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+		return 0;
+	}
+	case WM_KEYDOWN:
+	{
+		break;
+	}
+	case WM_KEYUP:
+	{
+		break;
+	}
+	case WM_LBUTTONDOWN:
+	{
+		break;
+	}
+	case WM_LBUTTONUP:
+	{
+		break;
+	}
+	case WM_RBUTTONDOWN:
+	{
+		break;
+	}
+	case WM_RBUTTONUP:
+	{
+		break;
+	}
+	case WM_MOUSEMOVE:
+	{
+		break;
+	}
+	}
+	return	DefWindowProc(hWnd, message, wParam, lParam);
 }
