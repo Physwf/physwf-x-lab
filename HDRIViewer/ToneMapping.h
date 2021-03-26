c77da8052e7e152df310bb79dffa680ca2a27a23
qiaoyuelei <qiaoyuelei@rsicsh.com>
2021-03-26 17:03:00 +0800

    reinhard

--- HDRIViewer/ToneMapping.h
+++ HDRIViewer/ToneMapping.h
@@ -2,6 +2,8 @@
 enum EToneMappingAlgo
 {
 	TMA_BILATERAL_FILTERING,
+	TMA_REINHARD,
+	TMA_REINHARD2,
 };
 
 void ToneMapping(const float* InputData, int Width, int Height, unsigned char* OutputData, EToneMappingAlgo Algo = TMA_BILATERAL_FILTERING);
