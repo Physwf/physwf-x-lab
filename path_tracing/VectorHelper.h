5ec503715293a14737d217af9b184cf202f5364e
physwf <joe.physwf@hotmail.com>
2021-07-16 17:22:14 +0800

    cross product bug

--- path_tracing/VectorHelper.h
+++ path_tracing/VectorHelper.h
@@ -41,7 +41,7 @@ public:
 	inline void Cross3(const T* lhs, const T* rhs, T* Result) const
 	{
 		Result[0] = lhs[1] * rhs[2] - lhs[2] * rhs[1];
-		Result[1] = lhs[0] * rhs[2] - lhs[2] * rhs[0];
+		Result[1] = lhs[2] * rhs[0] - lhs[0] * rhs[2];
 		Result[2] = lhs[0] * rhs[1] - lhs[1] * rhs[0];
 	}
 	template<typename U>
